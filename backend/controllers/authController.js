06502b6de44b95c7873d927f3be8b5d0ecb9835c
gourav <gouravsuthar564@gmail.com>
2026-03-14 16:15:19 +0530

    write authController and auth middleware code

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,15 +5,50 @@ import User from '../models/User.js';
 // Generate jwt token
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '7d',
+        expiresIn: process.env.JWT_EXPIRE || '7d',
     });
-}
+};
 
 // @desc    Register a new user
 // @route   POST /api/auth/register
+// @access Public
 export const register = async (req, res, next) => {
     try {
+        const { username, email, password } = req.body;
 
+        // Check if user exists
+        const userExists = await User.findOne({ $or: [{ email }] });
+        if (userExists) {
+            return res.status(400).json({
+                success: false,
+                error: userExists.email === email ? "Email already registered" : "Username already exists.",
+                statusCode: 400,
+            });
+        }
+
+        // Create New User
+        const user = await User.create({
+            username,
+            email,
+            password,
+        });
+
+        // Generate Token
+        const token = generateToken(user._id);
+        res.status(201).json({
+            success: true,
+            data: {
+                user: {
+                    id: user._id,
+                    username: user.username,
+                    email: user.email,
+                    profileImage: user.profileImage,
+                    createdAt: user.createdAt,
+                },
+                token,
+            },
+            message: "User registered successfully",
+        });
     } catch (error) {
         next(error);
     }
